31d75a397c1ed938f9f7f30c8b6e54596fe2a924
Shriya Palle <shripalle225@gmail.com>
2025-01-13 16:41:52 +0530

    working on DRF

--- demo2/settings.py
+++ demo2/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'rest_framework',
+    'django_livereload',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'demo2.urls'
